ff8ad651f149941cdf885904d5a337f348c8ba87
Percygu <380059082@qq.com>
2025-11-07 19:21:33 +0800

    场景题新增:mvcc

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -137,7 +137,7 @@ export default sidebar({
         "/backend_series/advanced_interview/database_index.md",
         "/backend_series/advanced_interview/sql_optimization.md",
        // "/backend_series/advanced_interview/database_lock.md",
-       // "/backend_series/advanced_interview/database_mvcc.md",
+        "/backend_series/advanced_interview/database_mvcc.md",
        "/backend_series/advanced_interview/slow_sql.md",
        "/backend_series/advanced_interview/java_request_slow.md",
         "/backend_series/advanced_interview/go_connection_pool.md",  
